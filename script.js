efb254463d82af33d8c636b8bd3efda729b06cf2
Tyerkal <94529171+Tyerkal@users.noreply.github.com>
2023-07-29 19:15:23 +0530

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const form = document.querySelector('form');
-const todoInput = document.querySelector('#todo-input');
-const todoList = document.querySelector('#todo-list');
+let inputbox = document.querySelector('#inputbox');
+let list = document.querySelector('#list');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  if (todoInput.value.trim() === '') {
-    return;
-  }
-  const todoItem = document.createElement('li');
-  todoItem.innerHTML = `
-    <span class="todo-item">${todoInput.value.trim()}</span>
-    <button class="delete-button">Delete</button>
-  `;
-  todoList.appendChild(todoItem);
-  todoInput.value = '';
+inputbox.addEventListener("keyup",function(event){
+    if(event.key == "Enter"){
+        addItem(this.value)
+        this.value = ""
+    }
+})
+
+let addItem = (inputbox) => {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `${inputbox}<i></i>`;
+
+     listItem.addEventListener("click", function(){
+        this.classList.toggle('done');
+     })
+
+     listItem.querySelector("i").addEventListener("click",function(){
+        listItem.remove();
+     })
+     
+      list.appendChild(listItem);
+}
